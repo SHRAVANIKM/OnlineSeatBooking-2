599d6462dc9eb6f00c8d4c95e206d214c2cc7376
shrakm <shrakm@INLEN8520027372.corp.capgemini.com>
2022-03-28 16:40:50 +0530

    changes

--- OnlineSeatBooking-2/src/main/java/com/OSB/Repository/SeatRepo.java
+++ OnlineSeatBooking-2/src/main/java/com/OSB/Repository/SeatRepo.java
@@ -3,8 +3,10 @@ package com.OSB.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.OSB.Model.Seat;
 
+@Repository
 public interface SeatRepo  extends JpaRepository<Seat, Integer> {
 
 	public Optional<Seat> findBylocationId(int locationId);
--- OnlineSeatBooking-2/src/main/java/com/OSB/Service/SeatService.java
+++ OnlineSeatBooking-2/src/main/java/com/OSB/Service/SeatService.java
@@ -3,9 +3,12 @@ package com.OSB.Service;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import com.OSB.Model.Seat;
 import com.OSB.Repository.SeatRepo;
 
+@Service
 @Component
 public class SeatService {
 
